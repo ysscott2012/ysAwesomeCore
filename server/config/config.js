e3682fe9d25b87d44f424a7b860bbfbd9b609c12
Yung-Sheng Chen <ysscott2012@gmail.com>
2017-07-30 00:25:14 -0400

    ysAwesomeCore

--- server/config/config.js
+++ server/config/config.js
@@ -11,11 +11,11 @@
 // Below two lines are the AWS Dev/Demo environment (developement Envirnoment)
 // TODO: add production to ENV configuration so we don't have to comment or uncomment lines
 // productionURL
-// process.env.PORT = 3000;
-// process.env.MONGODB_URI = 'srt:srt@ec2-54-145-198-134.compute-1.amazonaws.com:27017/Section508Dash';
+process.env.PORT = 3000;
+process.env.MONGODB_URI = 'ys:ys@ec2-34-225-194-59.compute-1.amazonaws.com:27017/ysAwesome';
 
 
 //  Local database setup (YS's local)
- process.env.PORT = 3000;
- process.env.MONGODB_URI = 'mongodb://localhost:27017/ysAwesome';
+//  process.env.PORT = 3000;
+//  process.env.MONGODB_URI = 'mongodb://localhost:27017/ysAwesome';
 
